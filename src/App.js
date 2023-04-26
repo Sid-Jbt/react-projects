950f9249c6e4d10a778b3afd8d2e6386178d6cfe
Sid-Jbt <siddharth.k@jarvisbitz.com>
2023-04-26 13:05:59 +0530

    Assignment - State Lifting Up

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
   const expenses = [
-    { title: 'Car Insurance', amount: 250, date: new Date(2022, 10, 4)},
-    { title: 'Bus Insurance', amount: 550, date: new Date(2020, 28, 4)},
-    { title: 'Bike Insurance', amount: 200, date: new Date(2020, 22, 4)},
-    { title: 'Other Insurance', amount: 750, date: new Date(2020, 20, 4)}
-  ]
+    { title: "Car Insurance", amount: 250, date: new Date(2022, 10, 4) },
+    { title: "Bus Insurance", amount: 550, date: new Date(2020, 28, 4) },
+    { title: "Bike Insurance", amount: 200, date: new Date(2020, 22, 4) },
+    { title: "Other Insurance", amount: 750, date: new Date(2020, 20, 4) },
+  ];
+
+  const addExpenseHandler = expense => {
+    console.log("In App Comp...");
+    console.log(expense);
+  }
   return (
     <div className="App">
-     <Expenses expenses = {expenses} />
+      <NewExpense onAddExpense = {addExpenseHandler}/>
+      <Expenses expenses={expenses} />
     </div>
   );
 }
